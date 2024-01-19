e88536f8269a37fbc81f944d962732024127d25b
nAach07 <ignacioduranch963@gmail.com>
2024-01-19 00:14:17 -0400

    Fin datos simples

---
+++ Ejercicios Básicos/ejer12.py
@@ -0,0 +1,6 @@
+print("PANADERIA \nUna barra de pan es 3.49$us")
+precio_pan=3.49
+n_pan=float(input("Cuantas hogasas de pan desea? \n"))
+pan_guardado = precio_pan*0.6
+print(f"Precio Total: {n_pan*precio_pan}")
+print(f"Precio por descuento {n_pan*pan_guardado}")
---
+++ Ejercicios Básicos/ejer6.py
@@ -0,0 +1,4 @@
+n = int(input("Escriba un numero entero positivo: "))
+suma =n*(n+1)/2
+print(f"la sumatoria de los numeros hasta {n} es {suma}")
+
